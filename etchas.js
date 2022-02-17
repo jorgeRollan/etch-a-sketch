b715f1821664fd01192b0a4298c2d6259c3049a7
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-17 19:25:44 +0100

    version funcional con las partes opcionales implementadas hace un limite de 100 cuadrados cambia de color aleatorio y decrece el brillo a cada paso

--- etchas.js
+++ etchas.js
@@ -1,69 +1,117 @@
-const gridDiv = document.querySelector(".gridDiv");
-gridDiv.style.setProperty('grid-template-columns', 'repeat(' + 16 + ', 100px)');
-gridDiv.style.setProperty('grid-template-rows', 'repeat(' + 16+ ', 100px)');
+//numero y tamano de los cuadrados del sketch
+const promptNumber =prompt("numero de cuadrados", "1");
+let squareNumber = null;
+if (!isNaN(promptNumber)){
+squareNumber= Number(promptNumber);
+}
+else squareNumber= 16;
+const squareSize = 100;
+const gridSize = 960;
 
 
-for (let a = 1; a <=16; a++) {
-    for (let b = 1; b<=16; b++) {
-        const divItem = document.createElement("div");
-        divItem.style.gridColumn = a + "/"+(a+1);
-        divItem.style.gridRow = b+ "/"+(b+1);
-        divItem.className = "" + a + b;
-        divItem.style.background="black";
-        bordersChange(a,b, divItem);
-        let trail=[];
-        divItem.addEventListener("mouseenter", function(event){
-            divItemChange(this);
-            console.log(event);
-            this.style.display="grid";
-            const trailItem=document.createElement("div")
-            this.style.setProperty('grid-template-columns', 'repeat(' + 100 + ', 1px)');
-            this.style.setProperty('grid-template-rows', 'repeat(' + 100 + ', 1px)');
-            trailItem.style.gridRow = (event.offsetY) + "/"+(event.offsetY+1);
-            trailItem.style.gridColumn = (event.offsetX) + "/"+(event.offsetX+1);
-            trailItem.style.background="black";
-            trailItem.className = "" + a + b;
-            trail.push(trailItem);
-            console.log(this)
-        });
-        divItem.addEventListener("mousemove",function(event){
-            trail.forEach(element => {
-                this.appendChild(element);
+//seleccion del div principal del body y pongo un boton de reset
+const body = document.querySelector("body");
+const button = document.createElement("button");
+button.textContent = "reiniciar tablero";
+button.addEventListener("click", function () {
+    resetTable();
+    drawEtch()
+})
+body.appendChild(button);
+drawEtch();
+//bucle que crea los items del grid y los coloca
+function drawEtch() {
+
+    const gridDiv = document.createElement("gridDiv");
+    gridDiv.id="gridDiv";
+    gridDiv.style.display = "grid";
+    gridDiv.style.width= gridSize + "px"
+    gridDiv.style.height= gridSize + "px"
+    gridDiv.style.setProperty('grid-template-columns', 'repeat(' + squareNumber + ', ' + gridSize/squareNumber + 'px)');
+    gridDiv.style.setProperty('grid-template-rows', 'repeat(' + squareNumber + ',' + gridSize/squareNumber + 'px)');
+
+    for (let a = 1; a <= squareNumber; a++) {
+        for (let b = 1; b <= squareNumber; b++) {
+
+            //propiedades del item nuevo
+            const divItem = document.createElement("div");
+            divItem.style.gridColumn = a + "/" + (a + 1);
+            divItem.style.gridRow = b + "/" + (b + 1);
+            divItem.className = "" + a + b;
+            divItemChange(divItem, false);
+
+            bordersChange(a, b, divItem);
+            let trail = [];
+
+            //evento de la entrada para cambiar el  color del cuadrado
+            divItem.addEventListener("mouseenter", function (event) {
+                divItemChange(this, true);
+                console.log(event);
+                this.style.display = "grid";
+                const trailItem = document.createElement("div")
+                console.log(divItem.clientWidth)
+                this.style.setProperty('grid-template-columns', 'repeat(' + divItem.clientWidth + ', 1px)');
+                this.style.setProperty('grid-template-rows', 'repeat(' + divItem.clientHeight + ', 1px)');
+                trailItem.style.gridRow = (event.offsetY) + "/" + (event.offsetY + 1);
+                trailItem.style.gridColumn = (event.offsetX) + "/" + (event.offsetX + 1);
+                trailItem.style.background = "black";
+                trailItem.className = "" + a + b;
+                trail.push(trailItem);
             });
-            const trailItem=document.createElement("div");
-            trailItem.style.gridRow = (event.offsetY) + "/"+(event.offsetY+1);
-            trailItem.style.gridColumn = (event.offsetX) + "/"+(event.offsetX+1);
-            trailItem.style.background="black";
-            trailItem.className = "" + a + b;
-            trail.push(trailItem);
-            console.log(this)
-       })
-       gridDiv.appendChild(divItem);
+
+            //evento del movimiento del raton que va mostrando la cola del mouse
+            divItem.addEventListener("mousemove", function (event) {
+                trail.forEach(element => {
+                    this.appendChild(element);
+                });
+                const trailItem = document.createElement("div");
+                trailItem.style.gridRow = (event.offsetY) + "/" + (event.offsetY + 1);
+                trailItem.style.gridColumn = (event.offsetX) + "/" + (event.offsetX + 1);
+                trailItem.style.background = "black";
+                trailItem.className = "" + event.offsetX + event.offsetY;
+                trail.push(trailItem);
+            })
+
+            divItem.addEventListener("mouseleave", function (event) {
+                //divItemChange(this, false);
+            });
+            gridDiv.appendChild(divItem);
+        }
+        body.appendChild(gridDiv);
     }
 }
 
-function divItemChange(divChange){
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    divChange.style.background="#" + randomColor;
-    //
+
+//funcion para cambiar el color del cuadrado de black a random y viceversa
+function divItemChange(divChange, random) {
+    if (random) {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        divChange.style.background = "#" + randomColor;
+    }
+    else divChange.style.background = "black";
 }
 
-function divItemChange2(divChange){
-    divChange.style.background="red";
+function divItemChange2(divChange) {
+    divChange.style.background = "red";
 }
 
 
-function bordersChange(a, b, div){
-    if (a==1){
-        div.style.borderLeft="solid grey";
+function bordersChange(a, b, div) {
+    if (a == 1) {
+        div.style.borderLeft = "solid grey";
     }
-    if(a==16){ 
-        div.style.borderRight="solid grey";
+    if (a == 16) {
+        div.style.borderRight = "solid grey";
     }
-    if (b==1){
-        div.style.borderTop="solid grey";
+    if (b == 1) {
+        div.style.borderTop = "solid grey";
     }
-    if(b==16){ 
-        div.style.borderBottom="solid grey";
+    if (b == 16) {
+        div.style.borderBottom = "solid grey";
     }
+}
+
+function resetTable(){
+    let div  = document.getElementById("gridDiv");
+        div.parentNode.removeChild(div);
 }
